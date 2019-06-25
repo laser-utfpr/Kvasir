e632c4711b3eafc4f430056368446b3c8f854a69
Gabriel Buiar <gabrielbuiar@gmail.com>
2019-06-25 15:40:27 -0300

    Added Robot.nop

--- Vor/source/main.cpp
+++ Vor/source/main.cpp
@@ -1,31 +1,37 @@
-#include "vor.hpp"
+//Este programa tem por objetivo captar as informacoes ditadas pela inteligencia artificial ou por um dispositivo de entrada e transmiti-las para os robos em campo
 
 #include <smmclib.hpp>
-
 #include <iostream>
 #include <string>
 
+#include "Vard.hpp"
+#include <QApplication>
+
 int main(int argc, char *argv[])
 {
-    if(argc > 3)
+    QApplication a(argc, argv);
+    if(argc > 1)
     {
-        bool right_key_sizes = true;
-        for(int i=1; i<4; i++)
+        if(argc == 4)
         {
-            std::string str(argv[i]);
-            if(str.size() != KEY_SIZE)
-                right_key_sizes = false;
-        }
-
-        if(right_key_sizes)
-        {
-            Vor vor(argv[1], argv[2], argv[3]);
-            return vor.runVor();
+            bool right_key_sizes = true;
+            for(int i=1; i<4; i++)
+            {
+                std::string str(argv[i]);
+                if(str.size() != KEY_SIZE)
+                    right_key_sizes = false;
+            }
+            if(right_key_sizes)
+            {
+                Vard vard(argv[1], argv[2], argv[3]);
+                vard.start();
+                std::cout << "Vard received shutdown signal";
+                return 0;
+            }
         }
     }
 
     std::cout << "ERROR: This application is expected to run from SMMCGUI receiving "
-              << "it's shared memory keys" << std::endl;
-
-    return 0;
+              << "it's shared memory keys or with -c for calibration" << std::endl;
 }
+
