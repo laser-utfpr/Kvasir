2a8a424dff5c7c29b75c1874924f9349341eb47a
Gabriel Rauta Buiar <gabrielbuiar@gmail.com>
2019-10-07 20:12:31 -0300

    Tested APF

--- Vor/source/strategy.cpp
+++ Vor/source/strategy.cpp
@@ -569,12 +569,21 @@ void Strategy::manualControl(void)
                 {
                     previous_manual_controlled_robot = -1;
                     previous_command = manual_command;
+                    
                 }
 
                 robot[n].destination = calculateDestination(n, robot[n].previous_destination.x - robot[n].coord.x, robot[n].previous_destination.y - robot[n].coord.y);
+                /*Coord t_vec = robot[n].coord - ball.coord;
+                Coord dest;
+                dest.x = (rg_ulc.x - robot[n].coord.x) + (t_vec.x)  * 500000 * (1/t_vec.norm() - 1/2) / (t_vec.norm() * t_vec.norm());
+                dest.y = (rg_ulc.y - robot[n].coord.y) + (t_vec.y)  * 500000 * (1/t_vec.norm() - 1/2) / (t_vec.norm() * t_vec.norm());
+                robot[n].destination = calculateDestination(n, dest.x - robot[n].previous_destination.x, dest.y - robot[n].previous_destination.y);*/
 
             }
         }
+        //Coord t_vec = robot[n].coord - ball.coord;
+        //std::cout << "att " << rg_ulc.x - robot[n].coord.x << " rep " << (t_vec.x)  * 500000 * (1/t_vec.norm() - 1/2) / (t_vec.norm() * t_vec.norm()) << std::endl;
+        //std::cout << "n: " << n << " x: " << robot[n].destination.x << " y: " << robot[n].destination.y << std::endl;
         robot[n].movement.angular_vel_scaling = 0;
     }
 }
